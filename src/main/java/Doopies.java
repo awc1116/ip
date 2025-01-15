0fbdc7dbd690ee0f312b7d1d5c7afc94f881bbdc
awc1116 <weicheng111602@gmail.com>
2025-01-15 18:56:16 +0800

    added echo functionality

--- src/main/java/Doopies.java
+++ src/main/java/Doopies.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Doopies {
     private static final String LINE = "_".repeat(60);
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String intro = String.format("""
                 %s
                 Hello! I'm Doopies
@@ -12,11 +15,31 @@ public class Doopies {
                 LINE);
 
         String end = String.format("""
+                %s
                 Bye. Hope to see you soon!
                 %s
                 """,
+                LINE,
                 LINE);
 
-        System.out.println(intro + end);
+        System.out.println(intro);
+
+        while (true) {
+            String in = sc.nextLine();
+            if (in.equalsIgnoreCase("bye")) {
+                System.out.println(end);
+                break;
+            } else {
+                System.out.printf("""
+                                %s
+                                %s
+                                %s
+                                %n""",
+                        LINE,
+                        in,
+                        LINE);
+            }
+        }
+        sc.close();
     }
 }
