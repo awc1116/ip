a882548f303240cef2036654b177d11a6725ce37
awc1116 <weicheng111602@gmail.com>
2025-02-20 19:05:39 +0800

    Edit README.md
    edited README.md to fit my bot's user guide

--- src/main/java/Doopies/userInterface/Ui.java
+++ src/main/java/Doopies/userInterface/Ui.java
@@ -17,55 +17,58 @@ public class Ui {
     private static final String HELP = """
             Command List
             ___________________________________________________________________________________________________________
-            
+
             Notes:
             1) Commands within {} are for user to fill up and {} can be ignored when typing in actual command.
-            
+
             2) When marking, unmarking or deleting tasks please call list to confirm the task number.
-            
+
             3) Current list will be stored in ../data/doopies.txt in chronological order
-               (todo tasks will be prioritise).
+            (todo tasks will be prioritise).
             ___________________________________________________________________________________________________________
             
             Add Tasks:
             1) todo task (no due date or no duration):
-            command: todo {description}
-            example: todo read book
-            
+               command: todo {description}
+               example: todo read book
+
             2) deadline task (task with a due date):
-            command: deadline /by {DD/MM/YYYY HHmm}
-            example: return book /by 20/02/2025 2359
+                command: deadline /by {DD/MM/YYYY HHmm}
+               example: return book /by 20/02/2025 2359
             
             3) event task (task with a period to do it):
-            command: event /from {DD/MM/YYYY HHmm} /to {DD/MM/YYYY HHmm}
+                command: event /from {DD/MM/YYYY HHmm} /to {DD/MM/YYYY HHmm}
+                example: event project meeting /from 21/2/2025 1400 /to /21/2/2025 1600
             ___________________________________________________________________________________________________________
             
             Viewing Tasks:
             1) view all task in task list:
-            command: list
-            
+                command: list
+
             2) view all undone todo task and deadline due within 24hrs or event starting within 24hrs:
-            command: reminders
+                command: reminders
+            ___________________________________________________________________________________________________________
+            
+            Managing Tasks:
+            1) mark task as done:
+                command: mark {task number on list}
+            
+            2) unmark task as done:
+                command: unmark {task number on list}
+            
+            3) delete task:
+                command: delete {task number on list}
             ___________________________________________________________________________________________________________
             
             Miscellaneous:
-            1) mark task as done:
-            command: mark {task number on list}
+            1) clear storage saved in hard disk:
+                command: clear
             
-            2) unmark task as done:
-            command: unmark {task number on list}
+            2) finding tasks using keyword(s) in task:
+                command: find {keyword(s)}
             
-            3) delete task:
-            command: delete {task number on list}
-            
-            4) clear storage saved in hard disk:
-            command: clear
-            
-            5) finding tasks using keyword(s) in task:
-            command: find {keyword(s)}
-            
-            6) shutdown the bot (app will close a second after this command):
-            command: bye\s
+            3) shutdown the bot (app will close a second after this command):
+                command: byes
             ___________________________________________________________________________________________________________
             """;
     private String message;
